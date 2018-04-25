ef0785def87d1d5e413778a30468579b6a6b329b
Chris Johnston <chris@thejohnstons.net>
2018-04-24 19:25:04 -0700

    remove functionality from main so that it is barebones

--- main.py
+++ main.py
@@ -1,182 +1,91 @@
 """
-Spotify Logger Application
+Spotify Demo Application
 
-Logs into spotify using the web api and a user token, and logs all playback activity to a database file.
+Logs into the spotify web api using the Spotipy library
+
+See the Spotipy docs here: https://spotipy.readthedocs.io/en/latest/
+These docs also provide more examples of sample bots, and what you can do
+using this library and the spotify web api.
+
+Installation:
+
+python3 -m pip install -r requirements.txt
+
+Usage:
+
+python3 main.py spotifyUserNameGoesHere
+
 """
 
 import sys
 import time
-
 import pprint
 
 # install from requirements.txt
+# using pip install -r requirements.txt
 import spotipy
 import spotipy.util
-import sqlite3
 
-# print(f'using spotipy version {spotipy.VERSION}')
-
-if len(sys.argv) > 2:
-    username = sys.argv[1]
-    db_path = sys.argv[2]
-else:
-    print("Usage: %s username db_path" % (sys.argv[0],))
-    sys.exit()
-
-# read access to a user's "your music" library
-# access to a user's top artist and tracks
-# read playback state
-# read currently playing track
-# read recently played items
-scopes = 'user-library-read, user-top-read, user-read-playback-state, user-read-currently-playing, user-read-recently-played'
-
-# have the following environment variables set
-
-# SPOTIPY_CLIENT_ID
-# SPOTIPY_CLIENT_SECRET
-
-# for this, just going to redirect back to localhost since it's not going to any app or anything
-
-user_token = spotipy.util.prompt_for_user_token(username, scopes, redirect_uri='http://localhost/')
-
-# log into the spotify web api now
-sp = spotipy.Spotify(auth=user_token)
-# debug
-# sp.trace = True
-# sp.trace_out = True
-
-# create tables if they don't exist already
-db = sqlite3.connect(db_path)
-
-# playback table - store information about user playback
-# including other tidbits of information that may be useful
-c = db.cursor()
-c.execute("""CREATE TABLE IF NOT EXISTS playback
-    (
-    timestamp UNSIGNED BIG INT,
-    
-    device_id TEXT,
-    device_name TEXT,
-    device_type TEXT,
-    device_volume_percent INT,
-    
-    track_id TEXT
-    
-    )""")
+if __name__ == '__main__':
+    # check the amount of arguments given
+    if len(sys.argv) > 1:
+        # if valid, use the first argument as the username
+        username = sys.argv[1]
+    else:
+        # print an error message when the input is invalid
+        print("Usage: %s username " % (sys.argv[0],))
+        sys.exit()
 
 
-# track details table - store information about track details and also track object info
-c.execute("""CREATE TABLE IF NOT EXISTS track_details
-    (
-    track_id TEXT,
-    
-    popularity INT,
-    name TEXT,
-    explicit BOOLEAN,
-    
-    acousticness REAL,
-    danceability REAL,
-    duration_ms UNSIGNED BIG INT,
-    energy REAL,
-    instrumentalness REAL,
-    key INT,
-    liveness REAL,
-    loudness REAL, 
-    mode INT,
-    speechiness REAL,
-    tempo REAL,
-    time_signature INT,
-    valence REAL
-    )""")
+    # Scopes are the different permissions that your application
+    # has granted to it by the users of the application
+    # Read more about scopes here: https://beta.developer.spotify.com/documentation/general/guides/scopes/
 
-# commit the changes when done
-db.commit()
+    # for example, to only use the 'user-library-read' scope, you would do
+    # scopes = 'user-library-read'
 
-while True:
-    # poll the api with details about user playback
-    current = sp.current_playback()
+    # to use multiple scopes, you will need a comma separated list of scopes like this
+    # scopes = 'user-library-read, user-top-read'
 
-    print('polling current_playback')
-    # print('current: ')
-    # pprint.pprint(current)
+    # users may be hesitant to allow any application access to their personal data
+    # so only include the scopes that you actually use
 
-    if current is not None and current['is_playing']:
-        # check to see if the current played song matches the most recently inserted one into the table
-        # if not, then count this as a new play of the song
-        # could do checking to see how long the song is being played, but for now will do a dumb insert
+    # need permission to get currently playing track
+    scopes = 'user-read-currently-playing'
 
-        # the timestamp will only change when the playback state changes (user plays, pauses, etc)
-        timestamp = current['timestamp']
-        # the item id of what's playing
-        item_id = current['item']['id']
+    # These environment variables either need to be set, or passed as parameters to the
+    # prompt_for_user_token method.
 
-        print(f'item_id {item_id}')
+    # SPOTIPY_CLIENT_ID
+    # SPOTIPY_CLIENT_SECRET
 
-        playback_obj = (timestamp,
-                        current['device']['id'],
-                        current['device']['name'],
-                        current['device']['type'],
-                        current['device']['volume_percent'],
-                        item_id)
+    # Normally, when you integrate your application with Spotify (think how Discord does it), the user
+    # will be taken to the spotify website, prompted if they want to enable the application, then
+    # redirected to a confirmation page.
+    # Because we aren't actually hosting a web server, or a full application, we are not going to have
+    # this redirect at all. Instead, set the redirect url to localhost. The prompt in the console
+    # is going to ask you to copy the contents of the address bar in the console anyways.
 
-        # get the last inserted object
-        c = db.cursor()
-        c.execute('SELECT track_id FROM playback ORDER BY timestamp DESC')
-        last_played = c.fetchone()
+    user_token = spotipy.util.prompt_for_user_token(username, scopes, redirect_uri='http://localhost/')
 
-        if last_played is not None and last_played[0] == item_id:
-            print('already played')
-            # pass
-        else:
-            print('logging playback obj')
-            # not already played, so log the playback_obj
-            c.execute("""INSERT INTO playback VALUES (?,?,?,?,?,?)""", playback_obj)
-            # commit to the db
-            db.commit()
+    # log into the spotify web api now
+    sp = spotipy.Spotify(auth=user_token)
 
-        # check if this track exists in the details db
-        # check first to avoid going back and forth to the api to get the track details
+    # Debug flags to set
+    # sp.trace = True
+    # sp.trace_out = True
 
-        c = db.cursor()
-        c.execute('SELECT * FROM track_details WHERE track_id=?', (item_id, ))
-        data = c.fetchone()
-        if data is None:
-            print('inserted new row into track details')
-            # doesn't exist in the table, so hit the details api to get track details
-            features = sp.audio_features(item_id)[0]
+    # loop forever
+    while True:
+        # use the web api to get information about the current user's playback
+        current_playback = sp.current_playback()
 
-            # print('features')
-            # pprint.pprint(features)
+        # display this information in the console
+        pprint.pprint(current_playback)
 
-            # make a track details obj
-            track_obj = (
-                item_id,
-                current['item']['popularity'],
-                current['item']['name'],
-                current['item']['explicit'],
-                features['acousticness'],
-                features['danceability'],
-                features['duration_ms'],
-                features['energy'],
-                features['instrumentalness'],
-                features['key'],
-                features['liveness'],
-                features['loudness'],
-                features['mode'],
-                features['speechiness'],
-                features['tempo'],
-                features['time_signature'],
-                features['valence'],
-            )
+        # wait for 5 seconds
 
-            # print(track_obj)
-
-            c.execute("""INSERT INTO track_details 
-             (track_id, popularity, name, explicit, acousticness, danceability, duration_ms, energy,
-             instrumentalness, key, liveness, loudness, mode, speechiness, tempo, time_signature, valence)
-             VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)""", track_obj)
-            db.commit()
-        else:
-            print('did not insert track details')
-    # only update every 5s or so
-    time.sleep(30)
+        # many APIs, Spotify included, implement a concept called 'rate limits'
+        # if you poll the same API too quickly, they may throttle your connection to the service
+        # to prevent spamming the server with too much traffic
+        time.sleep(5)
